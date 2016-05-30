25339b3b3fc110be91155c593320c25a02ea957d
fjw <fjw@garx.de>
2016-05-30 15:47:25 +0200

    onmyway

---
+++ packem.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+7z a shots.7z shots
+
+rm shots.7z
+cd shots
+rm *
--- shoot.sh
+++ shoot.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 cd /var/apps/screenshot
 phantom/bin/phantomjs shoot.js
-#7z a shots.7z shots
-
-
-
-#rm shots.7z
-#cd shots
-#rm *
